3894fd01db489ebc394071b5baec7ee7370a916d
Alexander <oorlov@sdl.com>
2014-08-07 20:59:29 +0300

    Fixed Bookmarklet + changed cdn path again.

--- Lite/Bookmarklet.js
+++ Lite/Bookmarklet.js
@@ -4,7 +4,7 @@
     {
     	var E = 0;
     	while (!(R = window.top.frames[E++].Tridion)) { }
-    	R = R.getApplicationWindow().document
+	    R = R.getApplicationWindow().document;
     }
     catch(ex)
     {
@@ -22,7 +22,8 @@
     E[e]('id',r); 
     E[e]('src',C+d);
     (R[a]('head')[0] || R[a]('body')[0]).appendChild(E); 
-})('createElement', 'setAttribute', 'getElementsByTagName', 'UIBeardcoreDevTools', 'uibc-lite.js', 'https://cnd.rawgit.com/UIBeardcore/UIBeardcore.DeveloperTool/master/Lite/', "script");
+})('createElement', 'setAttribute', 'getElementsByTagName', 'UIBeardcoreDevTools', 'uibc-lite.js', 'https://cdn.rawgit.com/UIBeardcore/UIBeardcore.DeveloperTool/master/Lite/', "script");
 //  B,              e,              a,                      r,                     d,              C,                        O,       R,      E
 
-(function (c, d, e, f, h, k, g, a, b) { try { for (b = 0; !(a = window.top.frames[b++].Tridion) ;); a = a.getApplicationWindow().document } catch (l) { alert("Not a Tridion!"); return } a.getElementById(f) || (b = (b = a[c + "NS"] && a.documentElement.namespaceURI) ? a[c + "NS"](b, g) : a[c](g), b[d]("id", f), b[d]("src", k + h), (a[e]("head")[0] || a[e]("body")[0]).appendChild(b)) })("createElement", "setAttribute", "getElementsByTagName", "UIBeardcoreDevTools", "uibc-lite.js", "https://cnd.rawgit.com/UIBeardcore/UIBeardcore.DeveloperTool/master/Lite/", "script");
+
+javascript:(function(B,e,a,r,d,C,O,R,E){try{var E=0;while(!(R=window.top.frames[E++].Tridion));R=R.getApplicationWindow().document}catch(ex){alert("Not a Tridion!");return}if(R.getElementById(r))return;E=R[B+"NS"]&&R.documentElement.namespaceURI;E=E?R[B+"NS"](E,O):R[B](O);E[e]("id",r);E[e]("src",C+d);(R[a]("head")[0]||R[a]("body")[0]).appendChild(E)})("createElement","setAttribute","getElementsByTagName","UIBeardcoreDevTools","uibc-lite.js","https://cdn.rawgit.com/UIBeardcore/UIBeardcore.DeveloperTool/master/Lite/","script");
